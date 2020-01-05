7278940832a6423c41762b7803962111a494de39
unknown <kobomoje_s_bamgboye@yahoo.com>
2020-01-05 22:08:58 +0100

    Updates

---
+++ precache-manifest.2cebfb1f7e22382984875ee2c91015d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c0543bab1962d7093ff4934b3f1e1f",
+    "url": "/gracias/index.html"
+  },
+  {
+    "revision": "58a96a4561cd5aa9a260",
+    "url": "/gracias/static/css/2.33062ccc.chunk.css"
+  },
+  {
+    "revision": "78edef4f3df90a1573a6",
+    "url": "/gracias/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "58a96a4561cd5aa9a260",
+    "url": "/gracias/static/js/2.ea6a3f22.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/gracias/static/js/2.ea6a3f22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "78edef4f3df90a1573a6",
+    "url": "/gracias/static/js/main.afac3253.chunk.js"
+  },
+  {
+    "revision": "890762514229d6c6fc3c",
+    "url": "/gracias/static/js/runtime-main.bfa5c8a7.js"
+  }
+]);
